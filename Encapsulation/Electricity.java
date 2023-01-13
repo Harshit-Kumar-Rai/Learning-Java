d21a08db5269602dc450fa4eee4b88183216f314
Harshit Kumar Rai <harshit.rai22@outlook.com>
2023-01-13 18:32:12 +0530

    Bill Payment

---
+++ Encapsulation/Electricity.java
@@ -0,0 +1,32 @@
+package Encapsulation;
+
+import java.util.Scanner;
+
+public class Electricity {
+    public static void main(String[] args) {
+
+        int bill = 0;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter Units:- ");
+        int unit  = input.nextInt();
+        input.close();
+        
+        if (unit > 0 && unit <=200) {
+            bill = 0;
+        }else if(unit>200 && unit<=500){
+            unit = unit - 200;
+            bill = unit * 4;
+            
+
+        }
+        else{
+            unit = unit - 200;
+            bill = unit * 6;
+            int less_price = 300 * 4;
+            bill = bill - less_price;
+        }
+        System.out.println("Total amount be paid :- "+bill);
+        
+    }
+}
